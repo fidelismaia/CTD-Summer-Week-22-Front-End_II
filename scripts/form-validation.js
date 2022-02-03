15e86a9342ea8ef097f6bd6a3522230767123b09
Lucas Fidelis Maia <85111490+fidelismaia@users.noreply.github.com>
2022-02-03 18:18:44 -0300

    aula01
    Aula 1

--- scripts/form-validation.js
+++ scripts/form-validation.js
@@ -6,7 +6,7 @@
   var forms = document.querySelectorAll('.needs-validation')
 
   // Loop over them and prevent submission
-  Array.prototype.slice.call(forms)
+  Array.prototype.slice.call(forms) 
     .forEach(function (form) {
       form.addEventListener('submit', function (event) {
         if (!form.checkValidity()) {
